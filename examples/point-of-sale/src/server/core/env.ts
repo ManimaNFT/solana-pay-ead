e50df1875eeec0ec5613db4f6ae03294b6699894
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2022-12-14 18:16:39 +0100

    Update env.ts

--- examples/point-of-sale/src/server/core/env.ts
+++ examples/point-of-sale/src/server/core/env.ts
@@ -1,3 +1,3 @@
-export const CLUSTER_ENDPOINT = process.env.CLUSTER_ENDPOINT || 'https://solana-mainnet.g.alchemy.com/v2/9y3Etc-VXZHiQnIhqWpAYbF3M9gychnf';
+export const CLUSTER_ENDPOINT = process.env.CLUSTER_ENDPOINT || 'https://solana-mainnet.phantom.tech';
 export const RATE_LIMIT = Number(process.env.RATE_LIMIT) || undefined;
 export const RATE_LIMIT_INTERVAL = Number(process.env.RATE_LIMIT_INTERVAL) || undefined;
