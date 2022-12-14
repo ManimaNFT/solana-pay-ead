1d2c869f015ae769916fe80248cf4293a019f2e8
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2022-12-14 17:39:43 +0100

    Update GenerateButton.tsx

--- examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
+++ examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
@@ -12,7 +12,7 @@ export const GenerateButton: FC = () => {
             onClick={generate}
             disabled={!amount || amount.isLessThanOrEqualTo(0)}
         >
-            Generate Payment Code
+            Generar código de pago
         </button>
     );
 };
