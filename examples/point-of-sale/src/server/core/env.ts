93758f7f8002d49cae184c326d5299e241a772fd
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2022-12-14 16:40:05 +0100

    Update env.ts

--- examples/point-of-sale/src/server/core/env.ts
+++ examples/point-of-sale/src/server/core/env.ts
@@ -1,3 +1,3 @@
-export const CLUSTER_ENDPOINT = process.env.CLUSTER_ENDPOINT || 'https://solana-mainnet.phantom.tech';
+export const CLUSTER_ENDPOINT = process.env.CLUSTER_ENDPOINT || 'https://solana-mainnet.g.alchemy.com/v2/v8O0wMuB-QYzIjESDPYKKcYHDKxjh6fY';
 export const RATE_LIMIT = Number(process.env.RATE_LIMIT) || undefined;
 export const RATE_LIMIT_INTERVAL = Number(process.env.RATE_LIMIT_INTERVAL) || undefined;
