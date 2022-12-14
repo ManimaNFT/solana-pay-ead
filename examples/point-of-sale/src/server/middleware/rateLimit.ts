f6489013bb9c5e3e32123bd1310b7f21f623eb81
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2022-12-14 17:23:33 +0100

    Update rateLimit.ts

--- examples/point-of-sale/src/server/middleware/rateLimit.ts
+++ examples/point-of-sale/src/server/middleware/rateLimit.ts
@@ -7,7 +7,7 @@ export const rateLimit = wrapExpressHandler(
     expressRateLimit({
         keyGenerator: (req) =>
             (req.headers['x-real-ip'] as string | undefined) ?? req.socket.remoteAddress ?? 'UNKNOWN',
-        max: RATE_LIMIT ?? 10,
+        max: RATE_LIMIT ?? 2,
         windowMs: RATE_LIMIT_INTERVAL ?? 1200,
     })
 );
