48ee2021489efd7f1424c9b4a44a4c542cc65ffe
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2022-12-14 18:44:15 +0100

    Update constants.ts

--- examples/point-of-sale/src/client/utils/constants.ts
+++ examples/point-of-sale/src/client/utils/constants.ts
@@ -8,11 +8,14 @@ export const NON_BREAKING_SPACE = '\u00a0';
 export const DEVNET_ENDPOINT = clusterApiUrl('devnet');
 
 // Use Phantom's mainnet endpoint instead of GenesysGo (auth issue) or default (ratelimits)
-export const MAINNET_ENDPOINT = 'https://rpc.ankr.com/solana';
+// export const MAINNET_ENDPOINT = 'https://rpc.ankr.com/solana';
 // export const MAINNET_ENDPOINT = 'https://solana-mainnet.g.alchemy.com/v2/9y3Etc-VXZHiQnIhqWpAYbF3M9gychnf';
 // export const MAINNET_ENDPOINT = 'https://solana-mainnet.phantom.tech';
 // export const MAINNET_ENDPOINT = 'https://api.mainnet-beta.solana.com';
+export const MAINNET_ENDPOINT = 'https://solana-api.projectserum.com';
+
 // Mint DUMMY tokens on devnet @ https://spl-token-faucet.com
+
 export const DEVNET_DUMMY_MINT = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr');
 
 export const MAINNET_USDC_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
