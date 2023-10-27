dcfe065a8bb08c51b27a4d7bc3b491f98b0d1ee5
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2023-10-27 21:39:14 +0200

    Update constants.ts

--- examples/point-of-sale/src/client/utils/constants.ts
+++ examples/point-of-sale/src/client/utils/constants.ts
@@ -1,6 +1,6 @@
 import { clusterApiUrl, PublicKey } from '@solana/web3.js';
 
-export const MAX_CONFIRMATIONS = 32;
+export const MAX_CONFIRMATIONS = 64;
 
 export const NON_BREAKING_SPACE = '\u00a0';
 
