c086be4d441dc9438d2dd87e32a702b06e480912
ManimaN <98038687+ManimaNFT@users.noreply.github.com>
2024-01-23 16:38:17 +0100

    Update App.tsx

--- examples/point-of-sale/src/client/components/pages/App.tsx
+++ examples/point-of-sale/src/client/components/pages/App.tsx
@@ -17,6 +17,7 @@ import { SOLIcon } from '../images/SOLIcon';
 import css from './App.module.css';
 import { MAINNET_ENDPOINT, MAINNET_EURC_MINT } from '../../utils/constants';
 //import { EURCIcon } from '../images/EURCIcon';
+import { USDCIcon } from '../images/USDCIcon';
 
 interface AppProps extends NextAppProps {
     host: string;
